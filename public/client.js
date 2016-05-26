3295cc6d857d6ef68197264e1d436f7e170c1194
ianeflint <ian.flint@icloud.com>
2016-05-26 00:07:50 -0400

    rotate camera

--- public/client.js
+++ public/client.js
@@ -3,6 +3,18 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 
+// rotates camera
+var lastNow = Date.now();
+viewer.clock.onTick.addEventListener(function(clock) {
+    var now = Date.now();
+    var spinRate = 0.05;
+    var delta = (now - lastNow) / 1000;
+    lastNow = now;
+    viewer.scene.camera.rotate(Cesium.Cartesian3.UNIT_Z, -spinRate * delta);
+});
+
+
+// sockets for moving real time updates between client and server
 var socket = io();
 socket.on('initial data', function (data) {
 	console.log(data.bro);
