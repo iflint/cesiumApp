1247ea4a163e99d93508356e8c6ac2756167dd9e
ianeflint <ian.flint@icloud.com>
2016-05-26 22:57:15 -0400

    select nearest point on rotate

--- public/client.js
+++ public/client.js
@@ -13,17 +13,44 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
   	accessToken: 'pk.eyJ1Ijoid29sdmVyaWFuMjMiLCJhIjoic3lSYnR0YyJ9.gwoi3oZpvvBRgntfVkXi9g'
   })
 });
+viewer.scene.fog.enabled = true;
 
 // utility functions
 function toggleRotate () {
 	var lastNow = Date.now();
+	var entityArray = viewer.entities.values;
+	var xCoordArray = [];
+	var currentEntity;
+	var smallestDiff;
+
+	for (i=0; i < entityArray.length; i++) {
+		var dd = entityArray[i].position._value;
+		var radianLon = Cesium.Cartographic.fromCartesian(dd).longitude;
+		xCoordArray.push(radianLon);
+	}
+
 	// function to remove the following listener is returned by the add lister function and stored in stopper
 	var stopper = viewer.clock.onTick.addEventListener(function(clock) {
 	  var now = Date.now();
 	  var spinRate = 0.03;
 	  var delta = (now - lastNow) / 1000;
 	  lastNow = now;
-	  console.log(viewer.scene.camera.position);
+
+	  var cX = viewer.scene.camera.positionCartographic.longitude;
+	  for (i=0; i < xCoordArray.length; i++) {
+	  	var nX = xCoordArray[i];
+	  	var thisDiff = Math.abs(nX - cX);
+	  	if (!smallestDiff || thisDiff < smallestDiff) {
+	  		if (currentEntity != entityArray[i].id) {
+	  			smallestDiff = thisDiff;
+	  			currentEntity = entityArray[i].id;
+	  			console.log(currentEntity, thisDiff, smallestDiff);
+	  			
+				  viewer.selectedEntity = viewer.entities.getById(currentEntity);
+				  console.log(viewer.selectedEntity);
+	  		}
+	  	}
+	  }
 	  viewer.scene.camera.rotate(Cesium.Cartesian3.UNIT_Z, -spinRate * delta);
 	});
 	return stopper;
@@ -40,12 +67,22 @@ $('.toggleRotate').on('click', function () {
 		stopThis();
 		rot = false;
 	}
-	
 });
 
 // sockets for moving real time updates between client and server
 var socket = io();
+
 socket.on('initial data', function (data) {
-	console.log(data.bro);
+	for (i=0; i < data.length; i++) {
+		viewer.entities.add({
+			name: data[i].name,
+			description: data[i].description,
+			position: Cesium.Cartesian3.fromDegrees(data[i].x, data[i].y),
+			point: {
+				color: Cesium.Color.fromCssColorString(data[i].color),
+				pixelSize: data[i].pixelSize
+			}
+		});
+	}
 });
 
--- server.js
+++ server.js
@@ -9,11 +9,27 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/globe.html')
 });
 
+var intialDataArray = [{
+	  name : 'Wyoming',
+	  description: '<div>lks dfjkls sdklf skldfj klsd</div>',
+	  x: 0,
+	  y: 0,
+	  color: 'red',
+	  pixelSize: 5
+	},
+	{
+	  name : 'dkjf',
+	  description: '<div>bruh</div>',
+	  x: 20,
+	  y: 0,
+	  color: 'red',
+	  pixelSize: 10
+	}
+];
+
 io.on('connection', function(socket) {
-	socket.emit('initial data', {
-		bro: 'hi'
-	});
-})
+	socket.emit('initial data', intialDataArray);
+});
 
 server.listen(3000, function () {
 	console.log('up and running')
